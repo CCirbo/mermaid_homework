af5a5d9e1536804894f61412a26969b203dd8461
Candice Cirbo <thecirbos@icloud.com>
2024-05-02 07:16:05 -0600

    Changed some of my wording for the test

--- test/mermaid_spec.rb
+++ test/mermaid_spec.rb
@@ -10,7 +10,7 @@ describe Mermaid do
 # start of it block for an individual test 
 # the string should briefly describe in plain English what is being tested 
   it 'is an instance of mermaid' do
-  # create a mermaid object instance 
+  # create a instance variable mermaid object 
   # since a mermaid object is being created from a Mermaid class, write a class named Mermaid
   # ALSO - since an argument is being passed to Mermaid, the initialized method needs to accept one
     mermaid = Mermaid.new('Sereia')
@@ -22,10 +22,8 @@ describe Mermaid do
   # the string should briefly describe in plain English what is being tested 
   it 'has a name' do
     mermaid = Mermaid.new('Sereia')
-    # start of it block for an individual test 
-    # the string should briefly describe in plain English what is being tested 
     # assert that the mermaid has a name property which matches what was passed in
-    # checking to see if the mermaid object instance has a name attribute that matches the 
+    # checking to see if the instance variable mermaid object has a name attribute that matches the 
     # value that was passed in upon creating the object instance 
     # since we need to call the name attribute and get back the string that was passed in, 
     # we need an attr_reader for the name attribute
@@ -36,17 +34,17 @@ describe Mermaid do
   # the string should briefly describe in plain English what is being tested
   it 'has an age' do
     mermaid = Mermaid.new('Sereia')
-    # asserts that the mermaid has an age property which has a default value of 22
+    # asserts that the mermaid has a variable instance age object which has an assigned value of 22
     # since we need to call the age attribute and get back the integer we need an attr_reader to see the age attribute 
     expect(mermaid.age).to eq 22
   end
-  
+
   # start of it block for an individual test 
   # the string should briefly describe in plain English what is being tested 
   it 'has can get older' do
     mermaid = Mermaid.new('Sereia')
-    # calling class method .get_older on a mermaid object
+    # calling class method .get_older on an instance variable mermaid object
     mermaid.get_older
-    # assert that mermaid age increase by 1 after calling class method get_older
+    # assert that the mermaid age increase by 1 after calling class method get_older 
     expect(mermaid.age).to eq 23
   end
